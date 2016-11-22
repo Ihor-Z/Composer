f689ef8df090369c633dfe694dfaa8f1a11e902f
Ihor_Z <ihor.zayats@gmail.com>
2016-11-22 17:45:27 +0200

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+layout 'first'
+  def first
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get 'pages/first'
+  get 'pages/second'
+
 end
